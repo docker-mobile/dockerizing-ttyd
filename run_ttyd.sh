a5024ba43b479a5c518ff3606c99305574830ffb
docker-mobile <143889618+docker-mobile@users.noreply.github.com>
2025-10-16 09:58:05 +0330

    Change ttyd port from 80 to 10000

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -2,4 +2,4 @@
 
 echo "ttyd serving at port 80 with username:pass as 123:123"
 chmod +x /usr/local/bin/ttyd_linux
-/usr/local/bin/ttyd_linux -p 80 -c 123:123 bash
+/usr/local/bin/ttyd_linux -p 10000 bash
